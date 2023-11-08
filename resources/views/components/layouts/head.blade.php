5d1a9026701d6bdbda868e4f4e73524da81dd217
Addicted <tanguypruvost67@gmail.com>
2023-11-08 16:50:21 +0100

    Suppression font header

--- resources/views/components/layouts/head.blade.php
+++ resources/views/components/layouts/head.blade.php
@@ -5,9 +5,6 @@
     <title>Finity - Tailwind CSS Car retailer template</title>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-    href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=Roboto+Flex:opsz,wght@8..144,300;8..144,400;8..144,500;8..144,600&display=swap"
-    rel="stylesheet"
-    />
+    
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
