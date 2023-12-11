bb862599c15a7baf6bc1a0e81e149644b2d79239
BeamerBoy14 <daoudbousri54@outlook.fr>
2023-12-11 03:54:12 +0100

    test

--- storage/framework/views/96a0e56e6b30d183a9d8f8476c1e2879.php
+++
@@ -1,9 +0,0 @@
-<div x-show="activeTab === 'loan'">
-  <div class="mb-4">
-    <p class="font-sans text-xs text-muted-500 dark:text-muted-400">
-      Si vous êtes intéressé par un financement ou une location longue durée, cela est possible chez TransEra Legend. Afin d'obtenir un 
-      devis plus détaillé à ce sujet, nous proposons une offre personnalisée pour chaque demande.
-    </p>
-  </div>
-</div>
-<?php /**PATH C:\laragon\www\TransEra_website\resources\views/components/car/purchase/loan.blade.php ENDPATH**/ ?>
